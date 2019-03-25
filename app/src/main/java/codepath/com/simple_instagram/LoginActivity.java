d37920f7dd4712695b39d73c8244411c1abdb745
YoTNT <pikaqiujian@gmail.com>
2019-03-24 21:08:05 -0400

    Version 1.0

--- app/src/main/java/codepath/com/simple_instagram/LoginActivity.java
+++ app/src/main/java/codepath/com/simple_instagram/LoginActivity.java
@@ -10,8 +10,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +32,23 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 login(username, password);
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signUp(username, password);
+                return;
             }
         });
     }
@@ -51,14 +65,37 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 // TODO: navigate to new activity if the user has signed properly
                 goMainActivity();
-                Log.e(TAG, "Login succeed!");
-                Toast.makeText(LoginActivity.this, "Login succeed!", Toast.LENGTH_LONG).show();
+                Log.d(TAG, "Login succeed!");
+                Toast.makeText(LoginActivity.this, "Login success!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void signUp(String username, String password){
+        ParseUser user = new ParseUser();
+
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Toast.makeText(LoginActivity.this, "Sign Up Success", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Log.e(TAG, "Sign up failed!");
+                    e.printStackTrace();
+                    return;
+                }
             }
         });
     }
 
     private void goMainActivity(){
+        Log.d(TAG, "Navigating to goMainActivity!");
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/codepath/com/simple_instagram/ParseApplication.java
+++ app/src/main/java/codepath/com/simple_instagram/ParseApplication.java
@@ -3,6 +3,7 @@ package codepath.com.simple_instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
@@ -10,12 +11,14 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Post.class);
+
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
         // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("jian-instagram-codepath") // should correspond to APP_ID env variable
                 .clientKey("CodepathInstagramMoveFast")  // set explicitly unless clientKey is explicitly configured on Parse server
-                .server("http://jian-instagram-codepath.herokuapp.com/parse").build());
+                .server("https://jian-instagram-codepath.herokuapp.com/parse").build());
     }
 }
